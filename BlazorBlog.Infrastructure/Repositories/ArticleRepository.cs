9ae2e8477909ced898a989997b10456dc7326c0f
Philip Tomlinson <phil@philt.dev>
2025-05-20 16:44:03 +0200

    Implemented the Article repository to retrieve data.

---
+++ BlazorBlog.Infrastructure/Repositories/ArticleRepository.cs
@@ -0,0 +1,26 @@
+﻿using BlazorBlog.Domain.Articles;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBlog.Infrastructure.Repositories;
+
+public class ArticleRepository : IArticleRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public ArticleRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Article>> GetAllArticlesAsync()
+    {
+        return await _context.Articles.ToListAsync();
+    }
+}
