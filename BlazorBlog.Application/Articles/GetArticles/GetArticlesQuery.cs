c56266e8ef9bebb0aaa186202130ed8354210827
Philip Tomlinson <phil@philt.dev>
2025-05-20 20:54:02 +0200

    Implemented a query and replaced the ArticleService.

---
+++ BlazorBlog.Application/Articles/GetArticles/GetArticlesQuery.cs
@@ -0,0 +1,15 @@
+﻿using BlazorBlog.Domain.Articles;
+
+using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBlog.Application.Articles.GetArticles;
+
+public class GetArticlesQuery : IRequest<List<Article>>
+{
+}
---
+++ BlazorBlog.Application/Articles/GetArticles/GetArticlesQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using BlazorBlog.Domain.Articles;
+
+using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBlog.Application.Articles.GetArticles;
+
+public class GetArticlesQueryHandler : IRequestHandler<GetArticlesQuery, List<Article>>
+{
+    private readonly IArticleRepository _articleRepository;
+
+    public GetArticlesQueryHandler(IArticleRepository articleRepository)
+    {
+        _articleRepository = articleRepository;
+    }
+
+    public async Task<List<Article>> Handle(GetArticlesQuery request, CancellationToken cancellationToken)
+    {
+        var articles = await _articleRepository.GetAllArticlesAsync();
+
+        return articles;
+    }
+}
