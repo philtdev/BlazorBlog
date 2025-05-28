4a0e75fb0e3a7c895033cef10641105963a6fce4
Philip Tomlinson <phil@philt.dev>
2025-05-28 16:12:55 +0200

    Added a Result object (part 1).

---
+++ BlazorBlog.Domain/Abstractions/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBlog.Domain.Abstractions;
+
+public class Result
+{
+    public bool Success { get; }
+    public bool Failure => !Success;
+    public string? Error { get; }
+
+    protected Result(bool success, string? errorMessage = null)
+    {
+        Success = success;
+        Error = errorMessage;
+    }
+
+    public static Result Ok() => new(true);
+
+    public static Result Fail(string errorMessage) => new(false, errorMessage);
+}
