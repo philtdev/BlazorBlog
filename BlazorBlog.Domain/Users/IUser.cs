cead335abaa7db616a0f3cb1d9b7bb65737e553b
Philip Tomlinson <phil@philt.dev>
2025-06-01 12:38:56 +0200

    Added a User interface and class.

---
+++ BlazorBlog.Domain/Users/IUser.cs
@@ -0,0 +1,11 @@
+﻿using BlazorBlog.Domain.Articles;
+
+namespace BlazorBlog.Domain.Users;
+
+public interface IUser
+{
+    public string Id { get; set; }
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
+    public List<Article> Articles { get; set; }
+}
---
+++ BlazorBlog.Infrastructure/Authentication/User.cs
@@ -0,0 +1,11 @@
+﻿using BlazorBlog.Domain.Articles;
+using BlazorBlog.Domain.Users;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorBlog.Infrastructure.Authentication;
+
+public class User : IdentityUser, IUser
+{
+    public List<Article> Articles { get; set; } = new List<Article>();
+}
