085444082a45c99c2a3fa90a539af281214b94f0
Philip Tomlinson <phil@philt.dev>
2025-07-24 11:28:12 +0200

    Fixed updating an article.

--- BlazorBlog.Application/Articles/UpdateArticle/UpdateArticleCommandHandler.cs
+++ BlazorBlog.Application/Articles/UpdateArticle/UpdateArticleCommandHandler.cs
@@ -1,24 +1,29 @@
-﻿namespace BlazorBlog.Application.Articles.UpdateArticle;
+﻿using BlazorBlog.Application.Users;
+
+namespace BlazorBlog.Application.Articles.UpdateArticle;
 
 public class UpdateArticleCommandHandler : ICommandHandler<UpdateArticleCommand, ArticleResponse?>
 {
     private readonly IArticleRepository _articleRepository;
+    private readonly IUserService _userService;
 
-    public UpdateArticleCommandHandler(IArticleRepository articleRepository)
+    public UpdateArticleCommandHandler(IArticleRepository articleRepository, IUserService userService)
     {
         _articleRepository = articleRepository;
+        _userService = userService;
     }
 
     public async Task<Result<ArticleResponse?>> Handle(UpdateArticleCommand request, CancellationToken cancellationToken)
     {
         var updatedArticle = request.Adapt<Article>();
 
+        if (!await _userService.CurrentUserCanEditArticleAsync(updatedArticle.Id))
+            return Result.Fail<ArticleResponse?>("You do not have permission to edit this article.");
+
         var article = await _articleRepository.UpdateArticleAsync(updatedArticle);
 
         if (article is null)
-        {
             return Result.Fail<ArticleResponse?>("The article does not exist.");
-        }
 
         return article.Adapt<ArticleResponse>();
     }
