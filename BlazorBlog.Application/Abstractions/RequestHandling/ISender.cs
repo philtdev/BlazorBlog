8e27659fcbb3e04d403582aa58bbf1f53b418e65
Philip Tomlinson <phil@philt.dev>
2025-07-26 13:14:50 +0200

    Implemented the Sender.

---
+++ BlazorBlog.Application/Abstractions/RequestHandling/ISender.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorBlog.Application.Abstractions.RequestHandling;
+
+public interface ISender
+{
+    Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+}
---
+++ BlazorBlog.Application/Abstractions/RequestHandling/Sender.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorBlog.Application.Abstractions.RequestHandling;
+
+public class Sender(IServiceProvider provider) : ISender
+{
+    public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+    {
+        var handlerType = typeof(IRequestHandler<,>).MakeGenericType(request.GetType(), typeof(TResponse));
+
+        dynamic handler = provider.GetRequiredService(handlerType);
+
+        return handler.Handle((dynamic)request, cancellationToken);
+    }
+}
